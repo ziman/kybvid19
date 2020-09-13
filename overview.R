6228c0cf502910849f86dd0ce8fd1a04c6917b25
Matus Tejiscak <ziman@functor.sk>
2020-09-13 14:58:54 +0200

    Add more plots.

--- overview.R
+++ overview.R
@@ -1,10 +1,17 @@
 library(tidyverse)
 source('shared.R')
 
-world <- read_csv('data/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv') %>%
-  rename(province=`Province/State`, country=`Country/Region`, lat=`Lat`, lon=`Long`) %>%
-  pivot_longer(-c(province, country, lat, lon), names_to='date', values_to='confirmed') %>%
-  mutate(date = lubridate::mdy(date))
+load <- function(fname, quantity) {
+  read_csv(fname) %>%
+    rename(province=`Province/State`, country=`Country/Region`, lat=`Lat`, lon=`Long`) %>%
+    pivot_longer(-c(province, country, lat, lon), names_to='date', values_to='confirmed') %>%
+    mutate(date = lubridate::mdy(date), quantity=quantity)
+}
+
+world <- rbind(
+  load('data/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', 'confirmed'),
+  load('data/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', 'deaths')
+)
 
 population <- tibble(
   country = c('Netherlands', 'Slovakia', 'Belgium', 'Czechia'),
@@ -13,7 +20,7 @@ population <- tibble(
 
 ggplot(
   world %>%
-    filter(is.na(province)) %>%
+    filter(is.na(province), quantity == 'confirmed') %>%
     inner_join(population, by='country') %>%
     arrange(country, date) %>%
     mutate(
@@ -44,7 +51,7 @@ ggsave('be-nl-cz-sk.png', dpi=96, width=8, height=6)
 
 ggplot(
   world %>%
-    filter(is.na(province)) %>%
+    filter(is.na(province), quantity=='confirmed') %>%
     filter(country == 'Netherlands') %>%
     inner_join(population, by='country') %>%
     arrange(country, date) %>%
@@ -73,3 +80,35 @@ ggplot(
 #theme_linedraw()
 
 ggsave('nl.png', dpi=96, width=5, height=4)
+
+ggplot(
+  world %>%
+    filter(is.na(province), quantity=='deaths') %>%
+    filter(country == 'Netherlands') %>%
+    inner_join(population, by='country') %>%
+    arrange(country, date) %>%
+    mutate(
+      c1m = 1e6 * confirmed / population,
+      delta = if_else(
+        country == lag(country),
+        c1m - lag(c1m),
+        NA_real_
+      ),
+      delta_7d = if_else(
+        lead(country, n=3) == lag(country, n=4),
+        (lead(c1m, n=3) - lag(c1m, n=4)) / 7,
+        NA_real_
+      )
+    ),
+  aes(date)
+) +
+  geom_line(aes(y = delta_7d), colour='red', size=1) +
+  geom_point(aes(y = delta), colour='#6dae42', size=.5) +
+  facet_wrap('country', ncol = 2, scales='free_y') +
+  xlab(NULL) +
+  ylab('new deaths per 1M population per day') +
+  scale_x_date(date_breaks = '1 month', date_labels = '%b') +
+  theme_kybcae
+#theme_linedraw()
+
+ggsave('nl-deaths.png', dpi=96, width=5, height=4)
